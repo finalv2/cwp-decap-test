a15e5535be7902d5e4cada7ee655de9cdd1e558e
Josh Rubenoff <1328849+jrubenoff@users.noreply.github.com>
2025-08-31 16:15:35 -0700

    Add randomly rotating logos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const navButton = document.querySelector("#nav-button");
 const navMobileMenu = document.querySelector("#mobile-menu");
+const logo = document.querySelector("#nav-logo");
 
 navButton.addEventListener("click", e => {
 	e.currentTarget.toggleAttribute("aria-expanded");
-	navMobileMenu.toggleAttribute("aria-hidden");
-  navMobileMenu.toggleAttribute("hidden");
+  navMobileMenu.toggleAttribute("data-hidden");
 });
+
+const randomLogo = () => `${Math.floor(Math.random() * 7) * 12.5}%`;
+
+logo.style.setProperty("--bgPosition", randomLogo() );
